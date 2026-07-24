1fee3b97a403b5aeb1047a0ce20ac47c6ec4e2c1
Dina Abdelghany <145467226+Dina-Elmowafy@users.noreply.github.com>
2026-07-25 02:45:30 +0300

    Add Employee Email application

---
+++ src/Spring Tasks/SpringTasklec41/EmployeeEmailManagement/src/main/java/com/dina/employeeemail/EmployeeEmailApplication.java
@@ -0,0 +1,12 @@
+package com.dina.employeeemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeEmailApplication.class, args);
+    }
+}
