077e4a3ff29b64e2aaf416582cdf6b6b0c08cf3e
Dina Abdelghany <145467226+Dina-Elmowafy@users.noreply.github.com>
2026-07-24 06:14:13 +0300

    Add Item model

---
+++ src/ItemServiceProject/src/main/java/com/item/model/Item.java
@@ -0,0 +1,57 @@
+package com.item.model;
+
+public class Item {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer totalNumber;
+
+    public Item() {
+    }
+
+    public Item(Long id, String name, Double price, Integer totalNumber) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.totalNumber = totalNumber;
+    }
+
+    public Item(String name, Double price, Integer totalNumber) {
+        this.name = name;
+        this.price = price;
+        this.totalNumber = totalNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getTotalNumber() {
+        return totalNumber;
+    }
+
+    public void setTotalNumber(Integer totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+}
