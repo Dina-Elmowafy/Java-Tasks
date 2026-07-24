788e9c07177ec72e554acbf5db4c2885eabfe6e9
Dina Abdelghany <145467226+Dina-Elmowafy@users.noreply.github.com>
2026-07-25 02:48:45 +0300

    Add EmployeeApi/src/main/java/com/student/employee/exception files

---
+++ src/Spring Tasks/SpringTasklec41/EmployeeApi/src/main/java/com/student/employee/exception/ApiError.java
@@ -0,0 +1,12 @@
+package com.student.employee.exception;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public record ApiError(
+        LocalDateTime timestamp,
+        int status,
+        String message,
+        Map<String, String> validationErrors
+) {
+}
