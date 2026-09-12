8f0cfe58dd56bb8460e66fc9492845b8eaa1b176
Dina Abdelghany <145467226+Dina-Elmowafy@users.noreply.github.com>
2026-09-12 03:23:13 +0300

    Delete src/Spring Tasks/EmployeeApi/src/main/java/com/student/employee/exception directory

--- src/Spring Tasks/EmployeeApi/src/main/java/com/student/employee/exception/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.student.employee.exception;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(Long id) {
-        super("Employee with id " + id + " was not found");
-    }
-}
